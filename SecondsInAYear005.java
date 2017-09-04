e3d8ade05748d0e800ec5b81435bf441e3f020ac
DanielSzpunar <danielszpunar@gmail.com>
2017-09-04 14:48:33 -0400

    helsinki-java-mooc-part1

---
+++ SecondsInAYear005.java
@@ -0,0 +1,6 @@
+public class SecondsInAYear005 {
+	public static void main(String[] args) {
+		int secondsInYear = 365 * 24 * 60 * 60;
+		System.out.println("There are " + secondsInYear + " seconds in a year.");
+	}
+}
