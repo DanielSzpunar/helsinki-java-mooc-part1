9820f13af252599c4afeace2927eae2e8dd9f858
DanielSzpunar <danielszpunar@gmail.com>
2017-09-04 14:33:15 -0400

    helsinki-java-mooc-part1

---
+++ HelloWorld002.java
@@ -0,0 +1,6 @@
+public class HelloWorld002 {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("And all the people of the world");
+	}
+}
---
+++ Spruce003.java
@@ -0,0 +1,10 @@
+public class Spruce003 {
+	public static void main(String[] args){
+	System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+	}
+}
