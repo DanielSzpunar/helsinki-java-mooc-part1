3802e8c4ee46a8ec3ba885df5c636a98dc0c366b
DanielSzpunar <danielszpunar@gmail.com>
2017-09-04 17:51:29 -0400

    helsinki-java-mooc-part1

---
+++ Addition006.java
@@ -0,0 +1,7 @@
+public class Addition006 {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 4;
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
+}
---
+++ Multiplication007.java
@@ -0,0 +1,7 @@
+public class Multiplication007 {
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 8;
+		System.out.println(num1 + " * " + num2 +  " = " +  (num1 * num2));
+	}
+}	
