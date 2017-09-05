ffee2702df2f84ec889a32a5e9b1ae15254b4ecf
DanielSzpunar <danielszpunar@gmail.com>
2017-09-05 15:13:51 -0400

    helsinki-java-mooc-part1

---
+++ LeapYear021.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear021 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(input.nextLine());
+        if (year % 4 == 0 && year % 100 !=0) {
+            System.out.println("The year is a leap year.");
+        }
+        else if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        }
+        else System.out.println("The year is not a leap year.");
+	}
+}
