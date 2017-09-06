926c26efb762850f95f3717851b6d7628c24cd8c
DanielSzpunar <danielszpunar@gmail.com>
2017-09-06 12:25:10 -0400

    helsinki-java-mooc-part1

---
+++ FromHundredToOne028.java
@@ -0,0 +1,10 @@
+public class FromHundredToOne028 {
+
+    public static void main(String[] args) {
+        int i = 100;
+        while(i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
