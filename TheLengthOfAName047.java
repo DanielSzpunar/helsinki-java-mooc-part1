b593c4822a6c9f76e9faf6cb0de707f13abefb3b
DanielSzpunar <danielszpunar@gmail.com>
2017-09-07 16:15:18 -0400

    helsinki-java-mooc-part1

---
+++ TheLengthOfAName047.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TheLengthOfAName047 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = input.nextLine();
+        System.out.println("Number of characters: " + name.length());
+
+    }
+}
