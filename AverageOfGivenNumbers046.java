b13c84a8b8bcb04bc6150215e2af45fece97175e
DanielSzpunar <danielszpunar@gmail.com>
2017-09-07 16:08:10 -0400

    helsinki-java-mooc-part1

---
+++ AverageOfGivenNumbers046.java
@@ -0,0 +1,13 @@
+public class AverageOfGivenNumbers046 {
+
+    public static void main(String[] args) {
+        double answer = average(4,3,6, 1);
+        System.out.println("Greatest: " + answer );
+    }
+    public static double average(int number1, int number2, int number3, int number4) {
+        return (double) sum(number1, number2, number3, number4) / 4;
+    }
+    public static int sum(int number1, int number2, int number3, int number4){
+        return number1 + number2 + number3 + number4;
+    }
+}
