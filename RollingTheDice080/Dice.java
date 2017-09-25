c6d381c1b199d0298a97db8465f939e758a724e4
DanielSzpunar <danielszpunar@gmail.com>
2017-09-24 21:44:25 -0400

    helsinki-java-mooc

---
+++ RollingTheDice080/Dice.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Dice {
+    private Random random;
+    private int numberOfSides;
+
+    public Dice(int numberOfSides) {
+        this.numberOfSides = numberOfSides;
+        random = new Random();
+    }
+    public int roll() {
+        return random.nextInt(this.numberOfSides) + 1;
+    }
+}
